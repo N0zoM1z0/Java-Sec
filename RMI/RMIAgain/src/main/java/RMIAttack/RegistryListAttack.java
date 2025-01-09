9f702b5e0b5f6817fa39df61a4620ff256e3ec43
N0zoM1z0 <1705004398@qq.com>
2025-01-09 22:37:05 +0800

    update RMI

---
+++ RMI/RMIAgain/src/main/java/RMIAttack/RegistryListAttack.java
@@ -0,0 +1,13 @@
+package RMIAttack;
+
+import RMIDemo.*;
+
+import java.rmi.Naming;
+
+public class RegistryListAttack {
+    public static void main(String[] args) throws Exception{
+        RemoteObj remoteObj = new RemoteObjImpl();
+        String[] s = Naming.list("rmi://127.0.0.1:1099");
+        System.out.println(s);
+    }
+}
